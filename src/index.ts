0b390c373eff81ef1f4e6e130094417fd39df9aa
Duc Nguyen <minhduc140583@gmail.com>
2021-04-25 00:10:15 +0700

    Optimize interface

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,6 @@ export interface ErrorMessage {
   param?: string|number|Date;
   message?: string;
 }
-export interface ResourceService {
-  resource(): any;
-  value(key: string, param?: any): string;
-  format(...args: any[]): string;
-}
-export interface LoadingService {
-  showLoading(firstTime?: boolean): void;
-  hideLoading(): void;
-}
 export interface HistoryProps {
   location: H.Location;
   history: H.History;
@@ -24,7 +15,7 @@ export interface MessageState {
   message: string;
 }
 export class MessageComponent<W extends HistoryProps, I extends MessageState> extends React.Component<W, I> {
-  constructor(props, protected resourceService: ResourceService, protected alertError: (m: string, title?: string, detail?: string, callback?: () => void) => void, protected loading?: LoadingService) {
+  constructor(props) {
     super(props);
     this.back = this.back.bind(this);
     this.name = this.name.bind(this);
@@ -33,15 +24,13 @@ export class MessageComponent<W extends HistoryProps, I extends MessageState> ex
     this.showError = this.showError.bind(this);
     this.showMessage = this.showMessage.bind(this);
     this.hideMessage = this.hideMessage.bind(this);
-    this.alertError = this.alertError.bind(this);
-    this.handleError = this.handleError.bind(this);
     this.ref = React.createRef();
   }
   protected _name = 'user';
   protected ref: any;
   protected form: any;
   protected alertClass = '';
-  protected running = false;
+  protected running: boolean;
 
   protected back(event: any): void {
     if (event) {
@@ -88,40 +77,4 @@ export class MessageComponent<W extends HistoryProps, I extends MessageState> ex
     this.alertClass = '';
     this.setState({ message: '' });
   }
-
-  handleError(err: any): void {
-    this.running = false;
-    if (this.loading) {
-      this.loading.hideLoading();
-    }
-    const data: any = err.response ? err.response : err;
-    const r = this.resourceService;
-    const title = r.value('error');
-    let msg = r.value('error_internal');
-    if (!err) {
-      this.alertError(msg, title);
-      return;
-    }
-    const status = data.status;
-    if (status && !isNaN(status)) {
-      msg = messageByHttpStatus(status, r);
-    }
-    this.alertError(msg, title);
-  }
-}
-
-export function messageByHttpStatus(status: number, r: ResourceService): string {
-  let msg = r.value('error_internal');
-  if (status === 401) {
-    msg = r.value('error_unauthorized');
-  } else if (status === 403) {
-    msg = r.value('error_forbidden');
-  } else if (status === 404) {
-    msg = r.value('error_not_found');
-  } else if (status === 410) {
-    msg = r.value('error_gone');
-  } else if (status === 503) {
-    msg = r.value('error_service_unavailable');
-  }
-  return msg;
 }
